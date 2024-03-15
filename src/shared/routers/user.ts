8ccd0eb29fd85c92a16069600d59b03392165d07
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-16 01:54:31 +0200

    add users router

---
+++ src/shared/routers/user.ts
@@ -0,0 +1,58 @@
+import { publicProcedure, router } from "@src/trpc";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import { db } from "../db";
+import { users } from "../schema";
+
+export const userRouter = router({
+  create: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await db.insert(users).values({
+        name: input.name,
+      });
+    }),
+  get: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      await db.select().from(users).where(eq(users.id, input.id));
+    }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    return await db.select().from(users);
+  }),
+  update: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        newName: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await db
+        .update(users)
+        .set({
+          name: input.newName,
+        })
+        .where(eq(users.id, input.id));
+    }),
+  delete: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await db.delete(users).where(eq(users.id, input.id));
+    }),
+  deleteAll: publicProcedure.mutation(async ({ ctx }) => {
+    await db.delete(users);
+  }),
+});
