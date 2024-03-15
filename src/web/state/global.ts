93865eeac061283c5707a4eccb4742644385ea20
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-16 01:53:24 +0200

    import tyep

--- src/web/state/global.ts
+++ src/web/state/global.ts
@@ -1,6 +1,6 @@
 import { observable } from "@legendapp/state";
 import { persistObservable } from "@legendapp/state/persist";
-import { GlobalState } from "@shared/types";
+import type { GlobalState } from "@shared/types";
 
 export const globalState$ = observable<GlobalState>({
   colorMode: "dark",
