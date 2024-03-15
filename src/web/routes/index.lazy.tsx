9b4d167b400cdc4a8ae19cea052562f481084b8d
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-16 01:57:28 +0200

    add user example

--- src/web/routes/index.lazy.tsx
+++ src/web/routes/index.lazy.tsx
@@ -1,3 +1,4 @@
+import t from "@src/shared/config";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/" as never)({
@@ -5,12 +6,47 @@ export const Route = createLazyFileRoute("/" as never)({
 });
 
 function Index() {
+  const { mutate: createUser } = t.user.create.useMutation({
+    onSuccess: () => utils.user.invalidate(),
+  });
+  const { mutate: deleteUsers } = t.user.deleteAll.useMutation({
+    onSuccess: () => utils.user.invalidate(),
+  });
+  const { mutate: updateUser } = t.user.update.useMutation();
+  const { data: users } = t.user.getAll.useQuery();
+  const utils = t.useUtils();
+
   return (
     <div className="container mx-auto w-full flex flex-col gap-4">
       <div>
         <p className="text-xl font-bold">Juu Electron starter</p>
       </div>
-      
+
+      <div>
+        <p className="mb-1 font-semibold">tRPC + db example</p>
+        <div>
+          <div className="flex gap-2 items-center">
+            <button
+              onClick={() => createUser({ name: "test" })}
+              className="bg-card px-3 py-1 rounded border border-stone-700 hover:brightness-110"
+            >
+              add user
+            </button>
+            <button
+              onClick={() => deleteUsers()}
+              className="bg-card px-3 py-1 rounded border border-stone-700 hover:brightness-110"
+            >
+              delete users
+            </button>
+          </div>
+          <div>
+            {users?.map((u: any) => (
+              <div>{JSON.stringify(u)}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+
       <div>
         <p className="mb-1 font-semibold">Packages used</p>
         <div className="flex flex-wrap gap-2">
@@ -21,7 +57,9 @@ function Index() {
           <div className="bg-card px-3 py-1 rounded">Drizzle</div>
           <div className="bg-card px-3 py-1 rounded">Legendapp/state</div>
           <div className="bg-card px-3 py-1 rounded">Biome</div>
-          <div className="bg-card px-3 py-1 rounded">TanStack Query & Router</div>
+          <div className="bg-card px-3 py-1 rounded">
+            TanStack Query & Router
+          </div>
         </div>
       </div>
 
