2ce0994694e0e89c09eb3f083faf6cb1c77d00db
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-17 17:07:41 +0200

    store sqlite db to appdata

--- drizzle-helper/drizzle.config.ts
+++ drizzle-helper/drizzle.config.ts
@@ -1,9 +1,20 @@
 import type { Config } from "drizzle-kit";
+import path from "path";
+
+/** DB_LOCATION is the location where the electron app stores user data:
+ * app.getPath('userData')
+ * For example on windows this gives:
+ * userData The directory for storing your app's configuration files, 
+ * which by default is the appData directory appended with your app's name. 
+ * By convention files storing user data should be written to this directory, 
+ * and it is not recommended to write large files here because some 
+ * environments may backup this directory to cloud storage.
+ * 
+ * so on windows for example: C:\Users\You\AppData\Roaming\juu
+  */
 
 export default {
-  dbCredentials: {
-    url: "../juu.db"
-  },
+  dbCredentials: { url: path.join(process.env.DB_LOCATION as string, 'db.sqlite')},
   schema: "../src/shared/schema/*.ts",
   driver: "better-sqlite",
   out: "../.drizzle",
--- src/shared/db.ts
+++ src/shared/db.ts
@@ -1,6 +1,7 @@
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import * as schema from "./schema/index";
+import { app } from "electron";
 
-const sqlite = new Database("juu.db");
+const sqlite = new Database(`${app.getPath('userData')}/db.sqlite`);
 export const db = drizzle(sqlite, { schema });
