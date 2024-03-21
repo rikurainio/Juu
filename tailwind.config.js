106394644dac377f58cfb678598c84697b2499c4
Riku Rainio <76222709+rikurainio@users.noreply.github.com>
2024-03-21 18:01:34 +0200

    clean

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,13 @@
-import { createThemes } from 'tw-colors'
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class',
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}",],
-  theme: {
-    extend: {
-      colors: {
-        text: "var(--color-text)",
-        background: "var(--color-background)",
-        card: "var(--color-card)",
-        border: "var(--color-border)",
-        bgHover: "var(--color-bg-hover)",
-      },
+export const content = ["./src/**/*.{js,jsx,ts,tsx,html}",];
+export const theme = {
+  extend: {
+    colors: {
+      text: "var(--color-text)",
+      background: "var(--color-background)",
+      card: "var(--color-card)",
+      border: "var(--color-border)",
+      bgHover: "var(--color-bg-hover)",
     },
   },
-  plugins: [],
-  /*
-  plugins: [
-    createThemes({
-      dark: {
-        'bg': '#161718',
-        'text': '#bfbfbf',
-        'border': '#111111',
-      },
-      light: {
-        'bg': '#eeeeee',
-        'text': '#080808',
-        'border': '#cccccc',
-      }
-    })
-  ],
-  */
-}
-
+};
